f5794bb7feec515f7807f22f4eed5a7e698cb539
Aidnalev <55815710+Aidnalev@users.noreply.github.com>
2022-09-22 18:44:04 -0500

    4 files modified, 1 deleted

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
-
   items = this.cartService.getItems();
-  
+
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    phone: '',
+    address: '',
+  });
   constructor(
-    private cartService: CartService
-  ) { }
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Tu orden ha sido enviada.', this.checkoutForm.value);
+    window.alert('Tu orden ha sido enviada.');
+    this.checkoutForm.reset();
+  }
 }
